a6d57a300d0f31edd0a19730a622605a0fcae215
zhangpan <2744687886@qq.com>
2026-05-03 13:58:19 +0800

    添加 craft-plugin-dev 作为 peerDependencies

--- src/index.browser.ts
+++ src/index.browser.ts
@@ -1,25 +1,18 @@
-import { parseData, type ParsedData } from "./shared/parser.js";
-
-type ServiceFn = (...args: any[]) => any;
-type NotifyFn<T> = (data: T) => void;
-
-interface PublishFn {
-  <T>(name: string): NotifyFn<T>;
-  (name: string, service: ServiceFn): void;
-}
+import { type PublishFn } from 'craft-plugin-dev';
+import { parseData, type ParsedData } from './shared/parser.js';
 
 export type HWInfoUpdatedData = ParsedData;
 
 export function HWInfo(publish: PublishFn) {
-  const ws = new WebSocket("ws://localhost:15500");
+  const ws = new WebSocket('ws://localhost:15500');
 
-  const notifyHWInfoUpdated = publish<HWInfoUpdatedData>("onHWInfoUpdated");
+  const notifyHWInfoUpdated = publish<HWInfoUpdatedData>('onHWInfoUpdated');
 
   ws.onopen = () => {
-    console.log("HWInfo Services WebSocket connection established");
+    console.log('HWInfo Services WebSocket connection established');
   };
   ws.onerror = error => {
-    console.error("HWInfo Services WebSocket error:", error);
+    console.error('HWInfo Services WebSocket error:', error);
   };
 
   ws.onmessage = message => {
